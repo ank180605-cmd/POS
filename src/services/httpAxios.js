db1f73f8d222228619f6e9e16479f6aba63f0992
ank180605-cmd <ank180605@gmail.com>
2026-01-19 16:11:24 +0700

    Update httpAxios.js

--- src/services/httpAxios.js
+++ src/services/httpAxios.js
@@ -1,15 +1,1 @@
 
-const httpAxios = axios.create({
-  baseURL: 'http://localhost:8000/api', // URL backend Laravel
-});
-
-httpAxios.interceptors.request.use(config => {
-  const token = localStorage.getItem('pos_token');
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
-
-export default httpAxios;
-
