aa63486e7bc30ac2a17c2fc7381d31f714095214
ank180605-cmd <ank180605@gmail.com>
2026-01-19 16:12:05 +0700

    Update httpAxios.js

--- src/services/httpAxios.js
+++ src/services/httpAxios.js
@@ -1,1 +1,15 @@
+import axios from 'axios';
 
+const httpAxios = axios.create({
+  baseURL: 'http://localhost:8000/api',
+});
+
+httpAxios.interceptors.request.use(config => {
+  const token = localStorage.getItem('pos_token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export default httpAxios;
